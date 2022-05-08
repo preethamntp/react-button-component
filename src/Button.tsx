acc33170f41d2be4a9a0d427d1e92f33759f9df4
preethamntp <preethamntp@gmail.com>
2022-05-08 10:42:59 +0530

    first commit

---
+++ src/Button.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  border: 1px solid red;
+  cursor: pointer;
+  text-align: center;
+  transition: background 250ms ease-in-out, transform 150ms ease;
+  :active {
+    transform: scale(0.99);
+  }
+`;
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const Button = ({ children }: Props) => {
+  return <StyledButton>{children}</StyledButton>;
+};
