67ab0a119e091f90ca706aae360df5693bde64eb
preethamntp <preethamntp@gmail.com>
2022-05-08 13:49:15 +0530

    test for the npm publish

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from '../Button';
+
+export default {
+	title: 'Button',
+};
+
+export const Story1 = () => <Button>Click me</Button>;
+
+Story1.storyName = 'Button';
